397924490392161da73f78f8cbb3e7badd9a3a31
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-10-26 23:22:03 -0700

    Completed select horn Sound

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,17 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selection = document.getElementById('horn-select');
+  const expose = document.getElementById('expose');
+  const exposeChildren = Array.from(expose.children);
+  const image = exposeChildren.find(elm => elm.nodeName === 'IMG');
+  const audio = exposeChildren.find(elm => elm.nodeName === 'AUDIO');
+
+  // Set image and audio file paths when option is selected
+  selection.onchange = () => {
+    const valueName = selection.options[selection.selectedIndex].value;
+    image.setAttribute('src', `assets/images/${valueName}.svg`);
+    audio.setAttribute('src', `assets/audio/${valueName}.mp3`);
+  }
+
 }
