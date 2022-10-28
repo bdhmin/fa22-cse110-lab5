cd03b837759077c94f7de852086c845fe272f76a
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-10-27 22:23:28 -0700

    Added speech options

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,19 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speechSynthesis = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+
+  // Added all speech options
+  speechSynthesis.addEventListener('voiceschanged', (e) => {
+    speechSynthesis.getVoices().forEach(voice => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    });
+  })
+
 }
